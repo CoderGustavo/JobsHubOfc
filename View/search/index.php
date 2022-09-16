6b0f550e7938d0f4382424053ee73fdea379a863
Gustavo Ornaghi Antunes <gustavoornaghiantunes@gmail.com>
2022-09-15 21:04:18 -0300

    passando arquivos para .php, criando rotas e organizando views

---
+++ View/search/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("/View/assets/templates/head.html"); ?>
+    <title>Buscando por ...</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+    case '' :
+        require __DIR__ . '/View/inicio/index.php';
+        break;
+    case '/cadastro' :
+        require __DIR__ . '/View/register/index.php';
+        break;
+    case '/buscando' :
+        require __DIR__ . '/View/search/index.php';
+        break;
+    default:
+        http_response_code(404);
+        // require __DIR__ . '/View/404.php';
+        break;
+}
+?>
