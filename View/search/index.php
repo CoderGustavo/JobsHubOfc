4a13b2d72c615ca33452711238177dc2b3bee307
Pedro Godoy <pedro.godoy@fatec.sp.gov.br>
2022-09-15 22:19:37 -0300

    comecei a fazer o menu

--- View/search/index.php
+++ View/search/index.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include_once("/View/assets/templates/head.html"); ?>
+    <?php include_once("../assets/templates/head.html"); ?>
     <title>Buscando por ...</title>
 </head>
 <body>
-    
+    <div class="container-fluid">
+        <div class="hero">
+            <header class="container d-flex justify-content-between align-items-center">
+                <div>
+                    <img src="/View/assets/img/logo.png" alt="logo">
+                </div>
+                <div>
+                    <i class="fas fa-align-left"></i>
+                </div>
+            </header>
+        </div>
+        
+    </div>
 </body>
 </html>
