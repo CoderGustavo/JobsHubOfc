4d94d2f61360b44ac5f1fac6395edb89f7062840
João Vitor Moreira Mariano da Silva <105250845+joaomoreira616@users.noreply.github.com>
2022-09-26 22:22:41 -0300

    criação da página home

--- View/home/index.php
+++ View/home/index.php
@@ -3,9 +3,46 @@
 <head>
     <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/head.html");?>
     <title>Document</title>
+    <link rel="stylesheet" href="/View/assets/css/home/style.css">
 </head>
 <body>
     <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/hero.php");?>
     <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/navbar-mobile.php");?>
+    <div class="container-fluid homeemalta">
+        <h3>Áreas em alta</h3>
+        <img src="/View/assets/img/icons/homeemalta.svg">
+    </div>
+    <div class="slideemalta">
+        <div class="bg-color5 area">
+            <h4>Front-end</h4>
+            <div>
+                <img src="/View/assets/img/icons/iconemalta.svg">
+            </div>
+        </div>
+        <div class="bg-color5 area">
+            <h4>Front-end</h4>
+            <div>
+                <img src="/View/assets/img/icons/iconemalta.svg">
+            </div>
+        </div>
+        <div class="bg-color5 area">
+            <h4>Front-end</h4>
+            <div>
+                <img src="/View/assets/img/icons/iconemalta.svg">
+            </div>
+        </div>
+        <div class="bg-color5 area">
+            <h4>Front-end</h4>
+            <div>
+                <img src="/View/assets/img/icons/iconemalta.svg">
+            </div>
+        </div>
+        <div class="bg-color5 area">
+            <h4>Front-end</h4>
+            <div>
+                <img src="/View/assets/img/icons/iconemalta.svg">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
