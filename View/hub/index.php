a13e503be88026f304d17f589db10b89f9a7aa07
João Vitor Moreira Mariano da Silva <105250845+joaomoreira616@users.noreply.github.com>
2022-10-04 22:09:22 -0300

    Comecei a fazer a página HUB sistema swap

---
+++ View/hub/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/head.html");?>
+    <title>Hub</title>
+    <link rel="stylesheet" href="/View/assets/css/hub/style.css">
+</head>
+<body>
+    <div class="bg-dark1">
+        <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/header.html");?>
+        <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/navbar-mobile.php");?>
+    </div>
+    <div class="bg-color5 hub">
+        <div class="title">
+            <h1>Vaga para front-end</h1>
+        </div>
+    </div>
+</body>
+</html>
