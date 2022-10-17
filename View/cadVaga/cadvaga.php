b76bb16d044c67306ac3d278edd17a27c909d943
IgorTeodoroDEV <igorteodoro2409@gmail.com>
2022-10-16 23:29:23 -0300

    Foi consertado o Bug onde estava aparecendo todas as telas a direita

--- View/cadVaga/cadvaga.php
+++ View/cadVaga/cadvaga.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-    <title>Cadastro de Vagas</title>
-    <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/head.html");?>
-    <link rel="stylesheet" href="/View/assets/css/cadVagas/style.css">
-</head>
-<body>
+<html lang="en">
+    <head>
+        <title>Cadastro de Vagas</title>
+        <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/head.html");?>
+        <link rel="stylesheet" href="/View/assets/css/cadVagas/style.css">
 
-    <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/header-fixed.html");?>
-    <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/navbar-mobile.php");?>
-    <?php include_once("cadastro.html");?>
-    <?php include_once("salary.html");?>
-    <?php include_once("vecancy-type.html");?>
-    <?php include_once("date.html");?>
-    <?php include_once("vecancy-finish.html");?>
+        <script src="/Vendor/jQuery-Mask-Plugin-master/src/jquery.mask.js"></script>
+        <script src="/View/assets/js/cadVagas/main.js"></script>
+    </head>
+    <body>
 
-</body>
+        <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/header-fixed.html");?>
+        <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/navbar-mobile.php");?>
+        <?php include_once("cadastro.html");?>
+        <?php include_once("salary.html");?>
+        <?php include_once("vecancy-type.html");?>
+        <?php include_once("date.html");?>
+        <?php include_once("vecancy-finish.html");?>
 
-    <script src="/Vendor/jQuery-Mask-Plugin-master/src/jquery.mask.js"></script>
-    <script src="/View/assets/js/cadVagas/main.js"></script>
-
+    </body>
 </html>
