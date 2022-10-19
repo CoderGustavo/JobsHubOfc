6482bf463885aae6799532b4c975d2e5d5ec65c1
João Vitor Moreira Mariano da Silva <105250845+joaomoreira616@users.noreply.github.com>
2022-10-18 22:23:51 -0300

    Adicionei o conteúdo na parte hub

--- View/hub/index.php
+++ View/hub/index.php
@@ -9,8 +9,51 @@
     <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/header-fixed.html");?>
     <?php include_once($_SERVER['DOCUMENT_ROOT']."/View/assets/templates/navbar-mobile.php");?>
     <div class="hub">
-        <div class="title">
-            <h1>Vaga para front-end</h1>
+        <div class="container container-hub">
+            <div class="title">
+                <h1>Vaga para front-end</h1>
+                    <h4>Candidatos 5/10</h4>
+            </div>
+            <div class="sobre-vaga">
+                <h5>Sobre a vaga:</h5>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Se
+                    did dolor in turpis rutrum convallis id quis augue. Integer
+                    euismod sit amet arcu in pretium. Cras tincidunt malesuad
+                    a massa vulputate tempus. Praesent quis nisl a metus sagit
+                    tis rhoncus. Mauris sit amet rutrum ipsum, vel placerat sem
+                    Nunc luctus faucibus dapibus. Morbi at elit at tellus venena
+                    tis gravida. Quisque cursus rhoncus fringilla.
+    
+                    Cras dui neque, interdum ac aliquet vitae, dapibus vel quam
+                    Nullam neque nisi, sollicitudin ut metus a, efficitur ultrices
+                    elit. Integer mauris sapien, porta molestie sollicitudin vel 
+                </p> 
+            </div>
+            <div class="hab-obrigatorias">
+                <h5>Habilidades obrigatórias: </h5>
+                <div class="vaga-habs vaga-habs-hub">
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                </div>
+            </div>
+            <div class="hab-diferenciais">
+                <h5>Habilidades diferenciais: </h5>
+                <div class="vaga-habs vaga-habs-hub">
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                    <span>HTML</span>
+                </div>
+            </div>
+            <span class="title-combinacao">Esta vaga combina 95% com você</span>
+            <div class="bar-combinacao"></div>
         </div>
     </div>
     <div class="card-like">
